7fd6152c3562588d8f7f70e2f5ccac457c290f61
Nico Maker <nicola.marano02@gmail.com>
2023-12-22 16:22:01 +0100

    Aggiunta Grafico Dinamico

--- Collatz.js
+++ Collatz.js
@@ -1,26 +1,51 @@
+document.getElementById("bar-chart").style.display = "none";
+document.getElementById("img").style.display = "block";
+
+let dati = {
+    labels: [],
+    datasets: [{
+        label: 'Dati Collatz',
+        borderWidth: 1,
+        data: []
+    }],
+};
+
+let myChart = null;
+
 function CalcolaCollatz() {
     let numero = InserisciNumero();
 
-    if (isNaN(numero) || (numero < 0)){
-        let StampaRisultato = "Inserisci numero valido";
+    if (isNaN(numero) || numero < 0 || numero === "") {
+        let StampaRisultato = "Inserisci un numero valido";
         let stampapassaggi = "";
 
         document.getElementById("passaggi").innerHTML = stampapassaggi;
         document.getElementById("risultato").innerHTML = StampaRisultato;
-    }
-    else {
+
+        document.getElementById("bar-chart").style.display = "none";
+        document.getElementById("img").style.display = "block";
+    } else {
+        document.getElementById("bar-chart").style.display = "block";
+        document.getElementById("img").style.display = "none";
+
         let risultato = CalcolaRisultato(numero);
 
         let StampaRisultato = "K ---> " + risultato;
-        document.getElementById("risultato").innerHTML = StampaRisultato
+        document.getElementById("risultato").innerHTML = StampaRisultato;
     }
 }
+
 
 let InserisciNumero = () => parseInt(document.getElementById("numero").value);
 
 function CalcolaRisultato(numero) {
+    dati.labels = [];
+    dati.datasets[0].data = [];
 
     let k = 0, passaggi = 0, stampaHTML = "";
+
+    dati.labels.push(passaggi + 1)
+    dati.datasets[0].data.push(numero)
 
     while (numero > 1) {
         passaggi++;
@@ -37,6 +62,8 @@ function CalcolaRisultato(numero) {
             numero = 3 * numero + 1;
             k++;
         }
+        dati.labels.push(passaggi + 1)
+        dati.datasets[0].data.push(numero)
     }
 
     stampaHTML += " 1";
@@ -45,6 +72,27 @@ function CalcolaRisultato(numero) {
     stampaHTML += "<br>";
 
     document.getElementById("passaggi").innerHTML = stampaHTML;
+
+    if (myChart) {
+        myChart.destroy();
+    }
+
+    let config = {
+        type: 'bar',
+        data: dati,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    };
+
+    let ctx = document.getElementById('bar-chart').getContext('2d');
+    myChart = new Chart(ctx, config);
+
+    document.getElementById("img").style.display = "none";
 
     return k;
 }
